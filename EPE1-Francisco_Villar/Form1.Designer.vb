f73863c4a1a0dac0f87ac6442c6bcb1dfa939ad7
Francisco Villar <franciscovillar1108@gmail.com>
2024-09-06 05:38:32 -0400

    Agregar archivos de proyecto.

---
+++ EPE1-Francisco_Villar/Form1.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCargar = New System.Windows.Forms.Button()
+        Me.lblCargar = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cmbProductos = New System.Windows.Forms.ComboBox()
+        Me.txtDescripcion = New System.Windows.Forms.TextBox()
+        Me.txtPrecio = New System.Windows.Forms.TextBox()
+        Me.txtStock = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnCargar
+        '
+        Me.btnCargar.Location = New System.Drawing.Point(273, 65)
+        Me.btnCargar.Name = "btnCargar"
+        Me.btnCargar.Size = New System.Drawing.Size(179, 46)
+        Me.btnCargar.TabIndex = 0
+        Me.btnCargar.Text = "Cargar Productos"
+        Me.btnCargar.UseVisualStyleBackColor = True
+        '
+        'lblCargar
+        '
+        Me.lblCargar.AutoSize = True
+        Me.lblCargar.Location = New System.Drawing.Point(52, 80)
+        Me.lblCargar.Name = "lblCargar"
+        Me.lblCargar.Size = New System.Drawing.Size(139, 16)
+        Me.lblCargar.TabIndex = 1
+        Me.lblCargar.Text = "Boton de Carga C.Box"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(52, 136)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(157, 16)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Selecc. codigo. Producto"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(52, 232)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(611, 16)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Detalle Producto:   -------------------------------------------------------------" &
+    "-----------------------------------------------------            "
+        '
+        'cmbProductos
+        '
+        Me.cmbProductos.FormattingEnabled = True
+        Me.cmbProductos.Location = New System.Drawing.Point(273, 128)
+        Me.cmbProductos.Name = "cmbProductos"
+        Me.cmbProductos.Size = New System.Drawing.Size(179, 24)
+        Me.cmbProductos.TabIndex = 5
+        '
+        'txtDescripcion
+        '
+        Me.txtDescripcion.Location = New System.Drawing.Point(55, 318)
+        Me.txtDescripcion.Name = "txtDescripcion"
+        Me.txtDescripcion.ReadOnly = True
+        Me.txtDescripcion.Size = New System.Drawing.Size(189, 22)
+        Me.txtDescripcion.TabIndex = 6
+        '
+        'txtPrecio
+        '
+        Me.txtPrecio.Location = New System.Drawing.Point(273, 318)
+        Me.txtPrecio.Name = "txtPrecio"
+        Me.txtPrecio.ReadOnly = True
+        Me.txtPrecio.Size = New System.Drawing.Size(189, 22)
+        Me.txtPrecio.TabIndex = 7
+        '
+        'txtStock
+        '
+        Me.txtStock.Location = New System.Drawing.Point(485, 318)
+        Me.txtStock.Name = "txtStock"
+        Me.txtStock.ReadOnly = True
+        Me.txtStock.Size = New System.Drawing.Size(189, 22)
+        Me.txtStock.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(55, 282)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(158, 16)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Descripcion del Producto"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(273, 282)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(125, 16)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Precio del Producto"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(485, 282)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(120, 16)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Stock del Producto"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(701, 450)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtStock)
+        Me.Controls.Add(Me.txtPrecio)
+        Me.Controls.Add(Me.txtDescripcion)
+        Me.Controls.Add(Me.cmbProductos)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblCargar)
+        Me.Controls.Add(Me.btnCargar)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "EPE1-Francisco Villar Muñoz"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCargar As Button
+    Friend WithEvents lblCargar As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cmbProductos As ComboBox
+    Friend WithEvents txtDescripcion As TextBox
+    Friend WithEvents txtPrecio As TextBox
+    Friend WithEvents txtStock As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+End Class
---
+++ EPE1-Francisco_Villar/Form1.vb
@@ -0,0 +1,70 @@
+﻿Imports Excel = Microsoft.Office.Interop.Excel
+
+Public Class Form1
+
+    ' Ruta del archivo Excel
+    Private Const RutaArchivo As String = "C:\Users\franc\Desktop\Visual Studio 2022\EPE1-Francisco_Villar\EPE1-Francisco_Villar\Resources\Lista.xls"
+
+    ' Declarar variables a nivel de clase para almacenar los datos
+    Private productos As New List(Of Producto)
+
+    Private Sub btnCargar_Click(sender As Object, e As EventArgs) Handles btnCargar.Click
+        ' Cargar los datos del Excel al ComboBox
+        CargarDatosExcel()
+        ' Llenar el ComboBox con los ID de los productos
+        cmbProductos.DataSource = productos
+        cmbProductos.DisplayMember = "Id"
+        cmbProductos.ValueMember = "Id"
+        btnCargar.Enabled = False
+    End Sub
+
+    Private Sub cmbProductos_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbProductos.SelectedIndexChanged
+
+        ' Obtener el producto seleccionado
+        Dim productoSeleccionado As Producto = cmbProductos.SelectedItem
+        ' Mostrar los detalles del producto
+        txtDescripcion.Text = productoSeleccionado.Nombre
+        txtPrecio.Text = productoSeleccionado.Precio
+        txtStock.Text = productoSeleccionado.Stock
+
+    End Sub
+
+    Private Sub CargarDatosExcel()
+        ' Crear objetos Excel
+        Dim excelApp As New Excel.Application
+        Dim excelWorkbook As Excel.Workbook = excelApp.Workbooks.Open(RutaArchivo)
+        Dim excelSheet As Excel.Worksheet = CType(excelWorkbook.Sheets(1), Excel.Worksheet)
+
+        Dim row As Integer = 2 ' Empezar desde la segunda fila ya que la primera es encabezado
+        While Not excelSheet.Cells(row, 1).Value Is Nothing
+            Dim producto As New Producto
+            producto.Id = CInt(excelSheet.Cells(row, 1).Value) ' Columna A: Id
+            producto.Nombre = excelSheet.Cells(row, 2).Value.ToString() ' Columna B: Nombre
+            producto.Precio = CInt(excelSheet.Cells(row, 6).Value) ' Columna F: Precio
+            producto.Stock = CInt(excelSheet.Cells(row, 7).Value) ' Columna G: Stock
+            productos.Add(producto)
+            row += 1
+        End While
+
+        ' Cerrar y liberar objetos Excel
+        excelWorkbook.Close(False)
+        excelApp.Quit()
+
+        ReleaseObject(excelSheet)
+        ReleaseObject(excelWorkbook)
+        ReleaseObject(excelApp)
+    End Sub
+
+    ' Clase para representar un producto
+    Private Class Producto
+        Public Property Id As Integer
+        Public Property Nombre As String
+        Public Property Precio As Integer
+        Public Property Stock As Integer
+    End Class
+
+    ' Método para liberar objetos COM
+    Private Sub ReleaseObject(ByVal obj As Object)
+
+    End Sub
+End Class
